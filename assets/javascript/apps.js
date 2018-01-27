3746a85b4c23073b24ea8daf6028357be2b5f2c6
kimmybeee <kimariehavertybozman@gmail.com>
2018-01-26 16:36:39 -0800

    Getting my questions pulbished to html and figuring out how to organize answers

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,13 +1,9 @@
 
 //Sets up Begin button on the screen
-$(".button").html("<button>" + " Let us begin!! " + "</button>");
+$(".button").html("<button id='startBtn'>" + " Click Me To Begin!! " + "</button>");
 //When the start button gets pushed, run startGame function
 $(".button").on("click", startGame);
 
-
-
-//Where do I put this to make my button go away?????
-$(".button").style.visibility = "hidden";
 
 
 //When the done button gets pushed, run the stop function
@@ -23,17 +19,105 @@ var intervalGo;
 
 var clockRunning = false;
 
+//create an array of answers
+//use.attr = true and false to denote which goes to what score.
+var questions = [
+	{
+	"question": "In 'My Neighbor Totoro', what was Totoro's most frequently used animal transport",
+	"answer": "Cat Bus", 
+	"option1": "Monster Taxi", 
+	"option2": "Big Blue Bicycle", 
+	"option3": "Rat Car"
+	},
+	{
+	"question": "What was the name of Gary Oldman's character in 'True Romance'",
+	"answer": "Drexl", 
+	"option1": "Marvin", 
+	"option2": "Detroit", 
+	"option3": "Clarence"
+	},
+		
+ 	{
+	"question": "How many kids went on adventure together in 'The Goonies'", 
+	"answer": "Seven", 
+	"option1": "Eight", 
+	"option2": "Five", 
+	"option3": "Twelve"
+	}
+]; 
+
+var q4 = [
+	{
+	"question": "Ridley Scott's 'The Duellists' took place in what era",
+	"answer": "1800's",
+	"option1": "1600's",
+	"option2": "1500's",
+	"option3": "1700's"
+	}
+]; 
+		
+var q5 = [
+	{
+	"question": "In 'Pathfinder', what was Ghost's biological ethnicity",
+	"answer": "Norse",
+	"option1": "Native American",
+	"option2": "Asian",
+	"option3": "Hispanic"
+	}
+];
+
+var q6 = [
+	{
+	"question": "Which character in 2010's 'Tron' best represents the 'hidden Buddha'",
+	"answer": "Clu",
+	"option1": "Tron",
+	"option2": "Crom",
+	"option3": "Ram"
+	}
+];
+
+var q7 = [
+	{
+	"question": "Alabama Whitman also gets a mention in what famous Quentin Tarantino movie"]
+	"answer": "Reservoir Dogs",
+	"option1":
+	"option2":
+	"option3":
+	}
+];	
+
+
+		
+		
+	
+		
+		
+		
+		"Pulp Fiction",]
+	
+
 //Run function
- function startGame()	{
+function startGame()	{
 
 	if (!clockRunning)	{
 		clockRunning = true;
 		intervalGo = setInterval(decrement, 1000);
-	}	
-}
+	}
 
+	$("#startBtn").remove();
+	
+	for (var i = 0; i < questions.length; i++)	{
+		//create div
+		//create question
+		$("#action-box").html("<h3>" + questions[i].question + "?</h3>")
+		//for loop for answers using j (after add randomization)
+		//after for look append div to action box
+	}
+
+}
 function decrement()	{
 //Decrease second cound by 1
+console.log(number);
 	number--;
 //Show time remaining and decreasing seciond count on screen by adding it to html	
 	$("#timer").html("<h3>" + "Time Remaining: " + number + " seconds" + "<h3>");
@@ -42,12 +126,12 @@ function decrement()	{
 //..end the game
 	endGame();
 	}
-}
+};
 
-function endgame()	{
+function endGame()	{
 	clearInterval(intervalGo);
 	clockRunning = false;
-	
+
 	$("#action-box").html("<h2>" + "You are all done!!" + "<h2>");
 	$("#action-box").append("<h3>" + "Correct Answers: " + answerCorrect + "<h3>");
 	$("#action-box").append("<h3>" + "Incorrect Answers: " + answerCorrect + "<h3>");
