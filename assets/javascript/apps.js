93c104b81625a65ab1b78d73ac2dc2c995f1dfde
kimmybeee <kimariehavertybozman@gmail.com>
2018-01-26 23:52:54 -0800

    mostly done, stuck on question/answer generation loop

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -4,13 +4,8 @@ $(".button").html("<button id='startBtn'>" + " Click Me To Begin!! " + "</button
 //When the start button gets pushed, run startGame function
 $(".button").on("click", startGame);
 
-
-
-//When the done button gets pushed, run the stop function
-// $("done").on("click", endGame);
-
 //Timer starts at 130
-var number = 130;
+var number = 131;
 var answerCorrect = 0;
 var answerWrong = 0;
 var answerUn = 0;
@@ -52,10 +47,10 @@ var questions =	[
 	{
 	"question": "Ridley Scott's 'The Duellists' took place in what era",
 	"answer": [
-		"1800's", 
-		"1600's", 
-		"1500's", 
-		"1700's"
+		"Eighteen Hundreds", 
+		"Sixteen Hundreds", 
+		"Fifteen Hundreds", 
+		"Seventeen Hundreds"
 		]
 	},
 	{
@@ -86,9 +81,9 @@ var questions =	[
 		]
 	},
 	{
-	"question": "What 2017 big budget, blockbuster film did director Rian Johnson direct in addition to indie films 'Brick' and 'Looper'",
+	"question": "What big budget, blockbuster film did Rian Johnson direct in addition to indie films 'Brick' and 'Looper'",
 	"answer": [
-		"Star Wars: The Last Jedi", 
+		"Star Wars - The Last Jedi", 
 		"Wonder Woman", 
 		"It", 
 		"Beauty and the Beast"
@@ -105,6 +100,8 @@ function startGame()	{
 	}
 //Makes game initilizing button disappear
 	$("#startBtn").remove();
+
+	generateQuestions();
 }
 
 function decrement()	{
@@ -114,17 +111,30 @@ function decrement()	{
 	$("#timer").html("<h3>" + "Time Remaining: " + number + " seconds" + "<h3>");
 //If time runs out.. 	
 	if (number === 0)	{
+		
 //..end the game
 	endGame();
 	}
 };
 
 function generateQuestions()	{
-//Assign the key of "true" to question.answer[0]
+//Assign "true" value to the first answer of the question - the correct answer
 	for (var t = 0; t < questions.length; t++)	{
-		var trueAnswer = questions.answer[0];
-		trueAnswer.attr("data-value", true)
-	}	
+		var trueAnswer = questions[t].answer[0];
+		$(trueAnswer).attr("data-value", true)
+	};
+
+//Assign "false" value to the other three answers to the question - the distractors
+	for (var f = 0; f < questions.length; f++)	{
+		var wrongAnswer1 = questions[f].answer[1];
+		$(wrongAnswer1).attr("data-value", false);
+
+		var wrongAnswer2 = questions[f].answer[2];
+		$(wrongAnswer2).attr("data-value", false);
+
+		var wrongAnswer3 = questions[f].answer[3];
+		$(wrongAnswer3).attr("data-value", false);
+	};	
 
 
 //For Loop(s) generates questions and answers and writed them to html	
@@ -132,19 +142,42 @@ function generateQuestions()	{
 		
 		//create question
 		$("#quiz").html("<h3>" + questions[i].question + "?</h3>")
-		// $("#quiz").append("<label")
+
 		//for loop for answers using j (after add randomization)
-		//after for look append div to action box
+		//append answers to question
+		$("#quiz").append("<h4>" + "<label class='radio-inline'>" + "<input type='radio' name='optradio'>" + " " + questions[i].answer + "</label>" + "</h4>");
+		
+		//if statement for picking answer whose value is true
+
+		//adds one to Correct answer tally
+		//answerCorrect++
+
+		//else adds one to Wrong answer tally
+		// answerWrong++
+
+		//if statement for no amswer
+
+		//adds one to Unanswer question tally
+		//answerUn++
+		
+		//
+		//Add done button
+		$("#done-button").html("<button id='doneBtn'>" + " I am all done!! " + "</button>");
+		//When the done button gets pushed, run the stop function
+		$("#done-button").on("click", endGame);
+
 	}
 }
 
 function endGame()	{
 	clearInterval(intervalGo);
 	clockRunning = false;
+	$("#quiz").remove();
+	$("#done-button").remove();
 
 	$("#action-box").html("<h2>" + "You are all done!!" + "<h2>");
 	$("#action-box").append("<h3>" + "Correct Answers: " + answerCorrect + "<h3>");
-	$("#action-box").append("<h3>" + "Incorrect Answers: " + answerCorrect + "<h3>");
-	$("#action-box").append("<h3>" + "Unanswered Questions: " + answerCorrect + "<h3>");
+	$("#action-box").append("<h3>" + "Incorrect Answers: " + answerWrong + "<h3>");
+	$("#action-box").append("<h3>" + "Unanswered Questions: " + answerUn + "<h3>");
 
 }
